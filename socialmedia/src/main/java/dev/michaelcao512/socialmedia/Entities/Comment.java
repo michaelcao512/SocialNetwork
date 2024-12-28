29f4f35add9477f16da134f6220cbf36a7ab89a5
Michael Cao <mcao124@bu.edu>
2024-12-27 19:26:16 -0500

    comment enttiy and controller / service modified to allow creating replies to self; delete doesn't work; also breaks viewing posts because infinite recursion

--- socialmedia/src/main/java/dev/michaelcao512/socialmedia/Entities/Comment.java
+++ socialmedia/src/main/java/dev/michaelcao512/socialmedia/Entities/Comment.java
@@ -1,15 +1,20 @@
 package dev.michaelcao512.socialmedia.Entities;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity
@@ -19,20 +24,27 @@ public class Comment {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long commentId;
 
-
     @ManyToOne
     @JoinColumn(name = "accountId", nullable = false)
-    @JsonBackReference (value = "account-comments")
+    @JsonBackReference(value = "account-comments")
     private Account account;
 
-
     @ManyToOne
-    @JoinColumn(name = "postId", nullable = false)
-    @JsonBackReference (value = "post-comments")
+    @JoinColumn(name = "postId")
+    @JsonBackReference(value = "post-comments")
     private Post post;
 
     private String content;
 
     private LocalDateTime dateCreated = LocalDateTime.now();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parentCommentId")
+    @JsonBackReference(value = "replies")
+    private Comment parentComment;
+
+    @OneToMany(mappedBy = "parentComment", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference(value = "replies")
+    private Set<Comment> replies;
 
 }
