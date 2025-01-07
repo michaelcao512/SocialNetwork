0629b7920d17caca94395be4de5404414ced16ad
Mitchell <mitchell.ramos1028@gmail.com>
2025-01-07 15:35:20 -0500

    add avatars to all users and user searches, fix small issue with search labels, add small css

--- frontend/src/Components/Profile/User/User.jsx
+++ frontend/src/Components/Profile/User/User.jsx
@@ -1,19 +1,30 @@
 import { Typography } from "@mui/material";
 import { StyledLink } from "../../../StyledComponents/StyledComponents";
+import "./user.css";
 
 function User(props) {
   const { user } = props;
-    
+
   return (
     <>
       <StyledLink
         destination={`/profile/${user.accountId}`}
         text={user.username}
       />
-      <Typography variant="body1" align="left" gutterBottom>
-        {" "}
-        {user.userInfo.firstName} {user.userInfo.lastName}{" "}
-      </Typography>
+      <div className="user-profiles">
+        <Avatar src={user?.avatarUrl || null}>
+          {user?.userInfo?.firstName?.charAt(0) || "#"}
+        </Avatar>
+        <Typography
+          id="user-fullname"
+          variant="body1"
+          align="left"
+          gutterBottom
+        >
+          {" "}
+          {user.userInfo.firstName} {user.userInfo.lastName}{" "}
+        </Typography>
+      </div>
     </>
   );
 }
